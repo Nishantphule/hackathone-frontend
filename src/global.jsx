025e10c927d0a7aaab3cd1f6b25878c1485f70d3
Nishantphule <nishantphule12@gmail.com>
2024-03-10 22:09:31 +0530

    api update

--- src/global.jsx
+++ src/global.jsx
@@ -1,3 +1,3 @@
-// export const API = "https://hackathone-backend.onrender.com";
+export const API = "https://hackathone-backend.onrender.com";
 
-export const API = "http://192.168.0.103:3001";
+// export const API = "http://192.168.0.103:3001";
